8feeb37c61f0d987768c7d4fdea4d3f67456bff8
Pankaj Tirpude <tirpudepankaj@gmail.com>
2021-04-03 15:08:24 +0530

    Implement str - str function

---
+++ ImplStrstr.java
@@ -0,0 +1,28 @@
+class ImplStrstr
+{
+
+  public static int strStr(String haystack, String needle)
+  {
+     if(haystack.equals(needle) || needle.length() == 0) return 0;
+
+     if(haystack.length() < needle.length()) return -1;
+
+
+     for(int i = 0; i < haystack.length(); i++)
+     {
+       int j = 1;
+       for(; j < needle.length(); j++)
+       {
+          if(i + j >= haystack.length()) return -1;
+          
+          if(haystack.charAt(i + j) != needle.charAt(j)) break;
+       }
+
+       if(j == needle.length()) return i;
+
+     }
+
+    return -1;
+  }
+
+}
