65db9bffe05618b4590e788ca2acabe03d3f7a9f
Pankaj Tirpude <tirpudepankaj@gmail.com>
2021-04-26 17:57:51 +0530

    LC #10 Regular Expression Matching DP Solution

---
+++ dynamic-programing/RegularExpressionMatchingDP.java
@@ -0,0 +1,54 @@
+
+enum Result{
+ TRUE,
+ FALSE
+}
+
+
+public class RegularExpressionMatchingDP
+{
+  
+  private Result[][] memo;
+
+   public boolean isMatch(String text, String pattern)
+   {
+      memo = new Result[text.length() + 1][pattern.length() + 1];
+      return dp(0, 0, text, pattern); 
+   }
+
+   private boolean dp(int i, int j, String text, String pattern)
+   {
+      if(memo[i][j] != null)
+        return memo[i][j] == Result.TRUE;
+      
+      boolean ans;
+      
+      if(j == pattern.length())
+        ans = i == text.length();
+      else
+      {
+         boolean firstMatch = (i < text.length() && (text.charAt(i) == pattern.charAt(j) || pattern.charAt(j) == '.'));
+         
+         if(j + 1 < pattern.length() && pattern.charAt(j + 1)  == '*' )
+         {
+            ans = dp(i, j + 2, text, pattern) || (firstMatch && dp(i + 1, j, text, pattern));
+         }
+         else
+         {
+            ans = firstMatch && dp(i + 1, j + 1, text, pattern);
+         }
+      }
+     
+      memo[i][j] = ans ? Result.TRUE : Result.FALSE;
+
+      return ans;
+     
+   }
+
+   public static void main(String args[])
+   {
+      RegularExpressionMatchingDP match = new RegularExpressionMatchingDP();
+
+      System.out.println(match.isMatch("aaaaaaaaaaaaaa", "a*"));
+   }
+}
