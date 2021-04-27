a0279f4301a118c96d530f11e77675ac58e623ce
Pankaj Tirpude <tirpudepankaj@gmail.com>
2021-04-27 12:48:52 +0530

    LC #32 Longest Valid Parentheses Solution

---
+++ dynamic-programing/LongestValidParentheses.java
@@ -0,0 +1,44 @@
+
+import java.util.Stack;
+
+public class LongestValidParentheses
+{
+
+  public int longestValidParentheses(String str)
+  {
+    int maxans = 0;
+    Stack<Integer> stack = new Stack<>();
+    stack.push(-1);
+   
+    for(int i = 0; i < str.length(); i++)
+    {
+      if(str.charAt(i) == '(')
+      {
+        stack.push(i);
+      }
+      else
+      {
+        stack.pop();
+  
+        if(stack.isEmpty())
+        {
+          stack.push(i);
+        }
+        else
+        {
+          maxans = Math.max(maxans, i - stack.peek());
+        }
+      }
+    }
+
+    return maxans;
+  }
+  
+  public static void main(String[] args)
+  {
+     LongestValidParentheses lvp = new LongestValidParentheses();
+     int ans = lvp.longestValidParentheses("((())");
+     System.out.println(ans);
+  }
+ 
+}
