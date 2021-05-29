783bef624173bc3c1f93920692bab9ff9e38c05d
Pankaj Tirpude <tirpudepankaj@gmail.com>
2021-05-29 19:24:45 +0530

    LC #172 |  Factorial Trailing Zeroes

---
+++ math/FactorialTrailingZeroes.java
@@ -0,0 +1,14 @@
+public class FactorialTrailingZeroes
+{
+   public int trailingZeroes(int n)
+   {
+     int cnt = 0;
+     while(n > 0)
+     {
+        cnt += n / 5;
+        n /=5;
+     }
+
+     return cnt;
+   }
+}
