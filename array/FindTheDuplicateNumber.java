0ba95862599135a2dae131f805b424e7903c6c2f
Pankaj Tirpude <tirpudepankaj@gmail.com>
2021-06-08 18:01:27 +0530

    LC #287 | Find the Duplicate Number

--- array/FindTheDuplicateNumber.java
+++ array/FindTheDuplicateNumber.java
@@ -1,13 +1,13 @@
 public class FindTheDuplicateNumber
 {
-   public int findDuplicate(int[] dups){
+   public int findDuplicate(int[] nums){
        int tortoise = nums[0];
        int hare = nums[0];
 
        do{
            tortoise = nums[tortoise];
            hare = nums[nums[hare]];
-       }while(tortoise != hare)
+       }while(tortoise != hare);
 
        tortoise = nums[0];
 
