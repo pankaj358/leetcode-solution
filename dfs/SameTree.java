fc888ee4faf118b3852274458e170e0e8aa3f5f7
Pankaj Tirpude <tirpudepankaj@gmail.com>
2021-06-27 04:44:49 +0530

    LC #100 | Same Tree

---
+++ dfs/SameTree.java
@@ -0,0 +1,20 @@
+public class SameTree{
+
+ public boolean isSameTree(TreeNode p, TreeNode q){
+
+   if(p == null) return q == null;
+   if(q == null) return p == null;
+
+   if(p.val != q.val) return false;
+
+   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+ }
+
+ class TreeNode {
+   TreeNode left;
+   TreeNode right;
+   int val;
+ }
+
+}
