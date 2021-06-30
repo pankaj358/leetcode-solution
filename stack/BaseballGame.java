65aee710fe1e672edb0746df731f10b3919ea624
Pankaj Tirpude <tirpudepankaj@gmail.com>
2021-07-01 02:29:00 +0530

    LC #682 | Baseball Game

---
+++ stack/BaseballGame.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+
+public class BaseballGame
+{
+  public int baseballGame(String[] ops)
+  {
+    Stack<Integer> stack = new Stack<>();
+
+    for(String op : ops)
+    {
+      if(op.equals("+"))
+      {
+        int top = stack.pop();
+        int newTop = top + stack.peek();
+        stack.push(top);
+        stack.push(newTop);
+      }
+      else if(op.equals("C"))
+      {
+        stack.pop();
+      }
+      else if(op.equals("D"))
+      {
+        stack.push(2 * stack.peek()); 
+      }
+      else
+      {
+        stack.push(Integer.valueOf(op));
+      }
+    }
+
+    int ans = 0;
+    for(int score : stack) ans+=score;
+
+    return ans;
+  }
+}
