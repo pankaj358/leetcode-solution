49d22f1a662cc868b0254eb3fdaf21c005f424ba
Pankaj Tirpude <tirpudepankaj@gmail.com>
2021-06-30 22:29:23 +0530

    LC #119 | Pascal's Triangle II

---
+++ dynamic-programing/PascalII.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class PascalII
+{
+  public List<Integer> pascalTriangle(int row) {
+
+     List<List<Integer>> ans = new ArrayList<>();   
+     ans.add(Arrays.asList(1));
+     for(int idx = 1; idx < row; idx++){
+        List<Integer> list = new ArrayList<>();
+        int prev = 0;
+        for(int val : ans.get(idx)){
+            list.add(prev + val);
+            prev = val;
+        }
+        list.add(1);
+        ans.add(list);
+     }
+
+     return ans.get(row);
+    
+  }
+}
