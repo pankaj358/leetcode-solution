ec0f0dd4f0467d720e516a3173b1e7fe88a5f847
Pankaj Tirpude <tirpudepankaj@gmail.com>
2021-07-14 23:43:37 +0530

    LC #367 | Valid Perfect Square

---
+++ math/IsPerfectSquare.java
@@ -0,0 +1,14 @@
+public class IsPerfectSquare
+{
+  public boolean isPerfectSquare(int x)
+  {
+     long  r = x;
+
+     while(r * r > x)
+     {
+         r = (r + x / r) / 2;
+     }
+
+     return r * r == x;
+  }
+}
