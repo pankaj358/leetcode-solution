529b0ceab461bd1a88130c71ee10509d1e7b19b9
Pankaj Tirpude <tirpudepankaj@gmail.com>
2021-08-03 10:23:14 +0530

    LC#152 | Maximum Product array

---
+++ algo/MaximumProductArray.java
@@ -0,0 +1,19 @@
+public class MaximumProductArray {
+
+  public int maximumProductArray(int []A) {
+    int N = A.length;
+    int res = A[0];
+
+    int l = 0;
+    int r = 0;
+
+    for(int idx = 0; idx < N; idx++) {
+      l = (l == 0 ? 1 : l) * A[idx];
+      r = (r == 0 ? 1 : r) * A[idx];
+      res = Math.max(res, Math.max(l, r));
+    }
+ 
+    return res;
+  }
+
+}
