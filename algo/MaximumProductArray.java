fc0719e14a149bc08a63c625971140aec59bbf7d
Pankaj Tirpude <tirpudepankaj@gmail.com>
2021-10-27 07:33:06 +0530

    LC#152 MaximumProductArray Correction for right product

--- algo/MaximumProductArray.java
+++ algo/MaximumProductArray.java
@@ -9,7 +9,7 @@ public class MaximumProductArray {
 
     for(int idx = 0; idx < N; idx++) {
       l = (l == 0 ? 1 : l) * A[idx];
-      r = (r == 0 ? 1 : r) * A[idx];
+      r = (r == 0 ? 1 : r) * A[N - idx - 1];
       res = Math.max(res, Math.max(l, r));
     }
  
